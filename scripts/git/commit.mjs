9212b39067677e464f098e0aceeb4f911233cc71
Awen-hub <920733056@qq.com>
2022-01-07 21:52:48 +0800

    featgit: 使用husky进行拦截并使用脚本自定义提交

---
+++ scripts/git/commit.mjs
@@ -0,0 +1,98 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { exec } from "child_process";
+const { log } = console;
+
+const promptList = [
+  {
+    type: "list",
+    message: `请选择一种coomit提交类型:
+      feat:      添加新功能,
+      fix:       修复bug,
+      refactor:  代码重构,
+      chore:     改变构建流程，新增依赖库、工具、构造工具的或者外部依赖的改动,
+      docs:      只改动了文档相关的内容,
+      style:     不影响代码含义的改动，例如去掉空格、改变缩进、增删分号,
+      perf:      提高性能的改动,
+      test:      添加测试或者修改现有测试,
+      ci:        自动化流程配置修改,
+      misc:      不属于以上任何一个分类
+    `,
+    name: "type",
+    choices: [
+      "feat",
+      "fix",
+      "refactor",
+      "chore",
+      "docs",
+      "style",
+      "perf",
+      "test",
+      "ci",
+      "misc",
+    ],
+    pageSize: 14,
+    loop: false,
+  },
+  {
+    type: "input",
+    message: "填写本次commit的影响范围(填写主要受影响的页面或组件等, 可选):",
+    name: "scope",
+  },
+  {
+    type: "input",
+    message: "填写本次commit内容的简要说明(必填):",
+    name: "subject",
+    validate(val) {
+      if (val === "") {
+        log(chalk.red("\n此项为必填项\n"));
+        return false;
+      }
+      if (val.length >= 50) {
+        log(chalk.red("\n提交说明应尽量简短，不能超过50个字符\n"));
+        return false;
+      }
+      return true;
+    },
+  },
+];
+
+const handleErr = (err, message) => {
+  if (message !== "") {
+    log(chalk.red(message));
+  }
+  log(err);
+  process.exit(1);
+};
+
+const handleResolve = (stdout, message, resolve) => {
+  log(chalk.blue(message));
+  log(stdout);
+  resolve();
+};
+
+const gitAdd = () =>
+  new Promise((resolve) => {
+    exec("git add .", (err, stdout) => {
+      if (err) {
+        handleErr(err, "git add . 执行出错");
+      }
+      handleResolve(stdout, "git add . 执行成功", resolve);
+    });
+  });
+
+const gitCommit = (type, scope, subject) =>
+  new Promise((resolve) => {
+    exec(`git commit -m "${type}${scope}: ${subject}" -n`, (err, stdout) => {
+      if (err) {
+        handleErr(err, "git commit执行出错");
+      }
+      handleResolve(stdout, "git commit执行成功", resolve);
+    });
+  });
+
+const { type, scope, subject } = await inquirer.prompt(promptList);
+
+await gitAdd();
+
+await gitCommit(type, scope, subject);
---
+++ scripts/git/push.mjs
@@ -0,0 +1,163 @@
+import compareVersions from "compare-versions";
+import inquirer from "inquirer";
+import { createRequire } from "module";
+import chalk from "chalk";
+import { writeFile } from "fs/promises";
+import dayjs from "dayjs";
+import { exec } from "child_process";
+const require = createRequire(import.meta.url);
+
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const { version: lastVersion } = require("./package");
+
+const { log } = console;
+
+const handleErr = (err, message) => {
+  if (message !== "") {
+    log(chalk.red(message));
+  }
+  log(err);
+  process.exit(1);
+};
+
+const handleResolve = (stdout, message, resolve) => {
+  log(chalk.blue(message));
+  console.log(stdout);
+  resolve();
+};
+
+const gitAdd = () =>
+  new Promise((resolve) => {
+    exec("git add .", (err, stdout) => {
+      if (err) {
+        handleErr(err, "git add . 执行出错");
+      }
+      handleResolve(stdout, "git add . 执行成功", resolve);
+    });
+  });
+
+const gitCommit = () =>
+  new Promise((resolve) => {
+    exec(
+      `git commit -m "misc: 修改package.json和AppLog.json" -n`,
+      (err, stdout) => {
+        if (err) {
+          handleErr(err, "git commit执行出错");
+        }
+        handleResolve(stdout, "git commit执行成功", resolve);
+      }
+    );
+  });
+
+const getBranch = () =>
+  new Promise((resolve) => {
+    exec("git rev-parse --abbrev-ref HEAD", (err, stdout) => {
+      if (err) {
+        handleErr(err, "获取当前分支名字时发生错误");
+      }
+      log(chalk.blue("成功获取当前分支名称"));
+      resolve(stdout.replace("\n", ""));
+    });
+  });
+
+const gitPush = (nowBranch) =>
+  new Promise((resolve) => {
+    exec(
+      `git push --set-upstream origin ${nowBranch} --no-verify --tags`,
+      (err, stdout) => {
+        if (err) {
+          handleErr(err, "git push发生错误");
+        } else {
+          handleResolve(stdout, "git push行成功", resolve);
+        }
+      }
+    );
+  });
+
+let promptList = [
+  {
+    type: "list",
+    message: "是否更新版本号:",
+    name: "isChangeVersion",
+    choices: ["Yes", "No"],
+    loop: false,
+  },
+];
+
+const { isChangeVersion } = await inquirer.prompt(promptList);
+
+if (isChangeVersion === "Yes") {
+  promptList = [
+    {
+      type: "input",
+      message: `请填写新的版本号(当前版本号${lastVersion}, 版本号格式必须为x.y.z形式)：`,
+      name: "versionNumber",
+      validate(version) {
+        if (!compareVersions.validate(version)) {
+          log(chalk.red("\n版本号不规范, 请重新填写\n"));
+          return false;
+        }
+        if (version.split(".").length !== 3) {
+          log(chalk.red("\n版本号格式必须为x.y.z形式, 请重新填写\n"));
+          return false;
+        }
+        if (compareVersions.satisfies(version, lastVersion)) {
+          log(chalk.red("\n当前版本号必须大于上一个版本号, 请重新填写\n"));
+          return false;
+        }
+        return true;
+      },
+    },
+  ];
+  const { versionNumber } = await inquirer.prompt(promptList);
+  promptList = [
+    {
+      type: "input",
+      message: `请填写更新日志(按一次回车键换行, 连续按两次回车键结束填写)：`,
+      name: "logContent",
+    },
+  ];
+
+  const logList = [];
+  const getLog = async (resolve) => {
+    const { logContent } = await inquirer.prompt(promptList);
+    if (logContent !== "") {
+      logList.push(logContent);
+      getLog(resolve);
+    } else {
+      resolve();
+    }
+  };
+
+  await new Promise((resolve) => {
+    getLog(resolve);
+  });
+
+  const packageJSON = require("./package");
+  packageJSON.nextVersion = versionNumber;
+  await writeFile("package.json", JSON.stringify(packageJSON, null, 2), {
+    flag: "w+",
+  });
+
+  const AppLog = require("./AppLog");
+  const time = dayjs.tz(dayjs(), "Asia/Shanghai").format("YYYY-MM-DD");
+  AppLog.data.push({
+    content: logList,
+    title: `${time} 新版本(V${versionNumber})`,
+  });
+  await writeFile("AppLog.json", JSON.stringify(AppLog, null, 2), {
+    flag: "w+",
+  });
+
+  await gitAdd();
+
+  await gitCommit();
+}
+
+const nowBranch = await getBranch();
+
+await gitPush(nowBranch);

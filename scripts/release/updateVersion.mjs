c02902c690dc6ddb22095d230742d3ba463d9271
Awen-hub <920733056@qq.com>
2022-01-08 20:07:18 +0800

    featlog: 使用脚本生成日志

---
+++ scripts/release/updateVersion.mjs
@@ -0,0 +1,22 @@
+import { createRequire } from "module";
+import { writeFile } from "fs/promises";
+import chalk from "chalk";
+
+const { log } = console;
+const require = createRequire(import.meta.url);
+const packageJSON = require("../../package");
+
+const { version, nextVersion } = packageJSON;
+
+if (version === nextVersion) {
+  log(chalk.blue("不需要更新package.json"));
+  process.exit(0);
+}
+
+packageJSON.version = nextVersion;
+
+await writeFile("package.json", JSON.stringify(packageJSON, null, 2), {
+  flag: "w+",
+});
+
+log(chalk.blue("package.json更新成功"))
